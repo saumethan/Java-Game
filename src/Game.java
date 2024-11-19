687dc7d4ec6fbc71a453057187f222185a533522
saumethan272 <e.saum@rgu.ac.uk>
2024-11-19 15:28:01 +0000

    created game class and added variables and method signatures

---
+++ src/Game.java
@@ -0,0 +1,41 @@
+/*
+ * @author Ethan Saum (2304185) @saumethan272 
+ * 19/11/2024
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game {
+
+    // Variables 
+    private Player player;
+    private Room room [][]; 
+    private ArrayList<ICHallenge> challenges;
+    private ArrayList<String> userCommands;
+    private ArrayList<ICommand> commands;
+
+    // Constructor
+    public Game() {
+        player = new Player();
+        challenges = new ArrayList<>();
+        userCommands = new ArrayList<>();
+        commands = new ArrayList<>();
+    }
+
+    public void startGame() {
+
+    }
+
+    public void executeCommands() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void summary() {
+
+    }
+}
