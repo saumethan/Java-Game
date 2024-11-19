8405cd0a18ca4954cfa501c29c11d87363573bcb
saumethan272 <e.saum@rgu.ac.uk>
2024-11-19 17:05:25 +0000

    added methods into class and changed the class name to upper case

--- src/puzzle.java
+++ src/puzzle.java
@@ -1,9 +1,35 @@
-public class puzzle {
+public class Puzzle implements IChallenge {
 
     // Variables 
-    private String queation;
+    private String description;
+    private String question;
     private String answer;
     private int attemps;
     private boolean isCompleted;
-    
+
+    public Puzzle(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+        attemps = 0;
+        isCompleted = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void attempt(int amount) {
+        attemps++;
+        if (amount == Integer.parseInt(answer)) {
+            isCompleted = true;
+        }
+    }
+
+    public Boolean isCompleted() {
+        return isCompleted;
+    }
 }
