3f619660bce93bc37d4bb2405d2f93ddcce4155f
saumethan272 <e.saum@rgu.ac.uk>
2024-11-24 20:27:02 +0000

    start of custom file reading class

---
+++ src/fileHandling/CustomFileReader.java
@@ -0,0 +1,39 @@
+package fileHandling;
+
+import challenges.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CustomFileReader {
+
+    public static ArrayList<IChallenge> readChallenges(String filePath) {
+
+        ArrayList<IChallenge> challenges = new ArrayList<>();
+
+        try {
+            File myFile = new File(filePath);
+            Scanner myFileReader = new Scanner(myFile);
+
+            while (myFileReader.hasNextLine()) {
+                String data = myFileReader.nextLine().trim();
+
+                String[] challengeItems = data.split(",", 3);
+                
+                String question = challengeItems[0].trim();
+                String answer = challengeItems[1].trim();
+
+                challenges.add(new Puzzle(question, answer));
+            }
+
+            myFileReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occourred");
+            e.printStackTrace();
+        }
+        return challenges;
+    } 
+}
